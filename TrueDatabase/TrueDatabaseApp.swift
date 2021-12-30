1bf9da3f28c366caf55382b74c3b5bb17e33eb4f
Darrien Huntley <dhuntleypro@gmail.com>
2021-12-30 04:29:32 -0500

    Initial Commit

---
+++ TrueDatabase/TrueDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueDatabaseApp.swift
+//  TrueDatabase
+//
+//  Created by Darrien Huntley on 12/30/21.
+//
+
+import SwiftUI
+
+@main
+struct TrueDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
